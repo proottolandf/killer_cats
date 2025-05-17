d4a194034242018b13445fc981d2bb252ff9eb1d
proottolandf <148494512+proottolandf@users.noreply.github.com>
2025-05-16 19:52:15 -0600

    arbol de animacion
    The blend tree is finished, however, there are problems with the animation transition due to an error that does not affect it. Now I will focus on the life and the enemies.

--- Assets/scrpts/CHARACTERCONTROLLER.cs
+++ Assets/scrpts/CHARACTERCONTROLLER.cs
@@ -10,17 +10,27 @@ public class CHARACTERCONTROLLER : MonoBehaviour
     public LayerMask capaSuelo;
     public AudioClip sonidoSalto;
     
+    //cosas tecnicas
     private Animator animator;
     private Rigidbody2D rigidBody;
     private BoxCollider2D boxCollider;
-    private bool mirandoDerecha = true;
     private float saltosRestantes;
+    
+    //animacion
+    private bool mirandoDerecha = true;
+    private Vector2 ultimaPosicion;
+    private Vector2 direccionMovimiento;
+    private float x;
+    private float y;
+    
+    
     private void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
         boxCollider = GetComponent<BoxCollider2D>();
         saltosRestantes = saltosMaximos;
         animator = GetComponent<Animator>();
+        ultimaPosicion = transform.position;
     }
 
     // Update is called once per frame
@@ -28,6 +38,8 @@ public class CHARACTERCONTROLLER : MonoBehaviour
     {
         ProcesarMovimiento();
         ProcesarSalto();
+        ActualizarDireccionMovimiento();
+        ActualizarAnimacion();
     }
 
     bool EstaEnSuelo()
@@ -40,8 +52,6 @@ public class CHARACTERCONTROLLER : MonoBehaviour
             0.2f,
              capaSuelo);
         return raycastHit.collider != null;
-       
-        animator.SetBool("isJumping", false);
     }
 
     void ProcesarSalto()
@@ -50,31 +60,19 @@ public class CHARACTERCONTROLLER : MonoBehaviour
         {
             saltosRestantes = saltosMaximos;
         }
-
-        if (Input.GetKeyDown("up") && saltosRestantes > 0)
+        if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space)) && saltosRestantes > 0)
         {
             saltosRestantes--;
             rigidBody.velocity = new Vector2(rigidBody.velocity.x, 0f);
             rigidBody.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
             AudioManager.Instance.ReproducirSonido(sonidoSalto);
         }
-       
     }
 
     void ProcesarMovimiento()
     {
         // Lógica de movimiento
         float inputMovimiento = Input.GetAxis("Horizontal");
-        
-          if(inputMovimiento != 0f)
-        {
-            animator.SetBool("isRunning", true);
-        }
-        else
-        {
-            animator.SetBool("isRunning", false);
-        }
-
         rigidBody.velocity = new Vector2(inputMovimiento * velocidad, rigidBody.velocity.y);
 
         GestionarOrientacion(inputMovimiento);
@@ -89,5 +87,33 @@ public class CHARACTERCONTROLLER : MonoBehaviour
             mirandoDerecha = !mirandoDerecha;
             transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
         }
+    }
+ void ActualizarDireccionMovimiento()
+    {
+        Vector2 posicionActual = transform.position;
+        direccionMovimiento = posicionActual - ultimaPosicion;
+
+        Vector2 direccionNormalizada = direccionMovimiento.normalized;
+
+        // Suavizado para evitar micro-movimientos molestos
+        float minMovimiento = 0.1f;
+        if (direccionMovimiento.magnitude > minMovimiento)
+        {
+            x = direccionNormalizada.x;
+            y = direccionNormalizada.y;
+        }
+        else
+        {
+            x = 0;
+            y = 0;
+        }
+
+        ultimaPosicion = posicionActual;
+    }
+
+    void ActualizarAnimacion()
+    {
+        animator.SetFloat("x", x);
+        animator.SetFloat("y", y);
     }
 }
